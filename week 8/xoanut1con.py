e07b0df06e2c95a1a2643a8e5cd14bd13489b84c
anphu1904 <24110258@vnu.edu.vn>
2025-10-30 12:48:15 +0700

    Create xoanut1con.py

---
+++ week 8/xoanut1con.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    if key < root.key:
+        root.left = insert(root.left, key)
+    elif key > root.key:
+        root.right = insert(root.right, key)
+    return root
+
+def inorder(root):
+    if root is None:
+        return []
+    return inorder(root.left) + [root.key] + inorder(root.right)
+
+def delete(root, key):
+    if root is None:
+        return None
+    if key < root.key:
+        root.left = delete(root.left, key)
+    elif key > root.key:
+        root.right = delete(root.right, key)
+    else:
+        if root.left is None:
+            return root.right
+        elif root.right is None:
+            return root.left
+    return root
+
+arr = [50, 30, 70, 40]
+root = None
+for x in arr:
+    root = insert(root, x)
+
+print("Trước khi xóa nút có 1 con (30):", inorder(root))
+root = delete(root, 30)
+print("Sau khi xóa nút có 1 con (30):", inorder(root))
